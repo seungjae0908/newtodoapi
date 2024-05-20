53d0856b8959be33ab54a1152fd5b2cc1d749ef0
kyoungmin <stephen4951@gmail.com>
2024-05-20 15:55:26 +0900

    #4. 회원 가입 처리

--- src/main/java/com/example/todo/userapi/api/UserController.java
+++ src/main/java/com/example/todo/userapi/api/UserController.java
@@ -1,12 +1,14 @@
 package com.example.todo.userapi.api;
 
+import com.example.todo.userapi.dto.request.UserSignUpRequestDTO;
+import com.example.todo.userapi.dto.response.UserSignUpResponseDTO;
 import com.example.todo.userapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -30,6 +32,38 @@ public class UserController {
         log.info("중복??? - {}", resultFlag);
         return ResponseEntity.ok().body(resultFlag);
     }
+
+    // 회원 가입 요청 처리
+    // POST: /api/auth
+    @PostMapping
+    public ResponseEntity<?> signUp(
+            @Validated @RequestBody UserSignUpRequestDTO dto,
+            BindingResult result
+    ) {
+        log.info("/api/auth POST! - {}", dto);
+
+        if (result.hasErrors()) {
+            log.warn(result.toString());
+            return ResponseEntity.badRequest()
+                    .body(result.getFieldError());
+        }
+
+        try {
+            UserSignUpResponseDTO responseDTO = userService.create(dto);
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+    // 로그인 요청 처리 메서드를 선언하세요.
+    // LoginRequestDTO 클래스를 생성해서 요청 값을 받아 주세요.
+    // 서비스로 넘겨서, 로그인 유효성을 검증하세요. (비밀번호 암호화되어 있어요.)
+    // 로그인 결과를 응답 상태 코드로 구분해서 보내 주세요.
+    // 로그인이 성공했다면 200, 로그인 실패라면 400을 보내주세요 (에러 메세지를 상황에 따라 다르게 전달해 주세요.)
+
 
 }
 
